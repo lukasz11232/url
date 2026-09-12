2909c2d26ab8c08e51b801ec595004f7d726d94d
admin <lukasz_wojs@vp.pl>
2026-09-12 14:15:39 +0200

    add Urls entity with JPA annotations for URL shortening application

---
+++ src/main/java/com/shorter/url/repository/Urls.java
@@ -0,0 +1,82 @@
+package com.shorter.url.repository;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.Objects;
+import java.util.UUID;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+@Getter
+@Setter
+
+
+@Entity
+@Table(name = "urls")
+public class Urls{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+
+    @Column(name = "short_url",nullable = false, length = 50,unique = true)
+    private String shortUrl;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Column(name = "access_count")
+    @Builder.Default
+    private Integer accessCount = 0;
+
+    @PrePersist
+   protected void onCreate(){
+        createdAt = Instant.now();
+        updatedAt = Instant.now();
+        if(accessCount == null){
+            accessCount = 0;
+        }
+    }
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = Instant.now();
+    }
+
+
+
+    @Override
+    public boolean equals(Object o){
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Urls urls = (Urls) o;
+        return Objects.equals(id,urls.id) && Objects.equals(url,urls.url) && Objects.equals(shortUrl,urls.shortUrl) && Objects.equals(createdAt,urls.createdAt) && Objects.equals(updatedAt,urls.updatedAt) && Objects.equals(accessCount,urls.accessCount);
+    }
+
+    @Override
+    public int hashCode(){
+        int result = Objects.hashCode(id);
+        return result;
+    }
+
+    @Override
+    public String toString(){
+        return "urls{" +
+                "id=" + id +
+                ", url='" + url + '\'' +
+                ", shortUrl='" + shortUrl + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                ", accessCount=" + accessCount +
+                '}';
+    }
+}
